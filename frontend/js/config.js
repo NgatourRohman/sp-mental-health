460afb7007065370895649f3c828f126824d6b78
Ngatour Rohman <ngatourrohman25@gmail.com>
2026-07-10 01:41:52 +0700

    Expose Supabase config globally

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,6 @@
-const CONFIG = {
+window.CONFIG = {
     SUPABASE_URL: 'https://tpjvdmgsgwbikemiefef.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwanZkbWdzZ3diaWtlbWllZmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1NTY3NTAsImV4cCI6MjA5OTEzMjc1MH0.e3Wt6feMDIVfXEsQJR_d41ujvp8LRYEtfpAgOviHik8'
 };
+
+const CONFIG = window.CONFIG;
